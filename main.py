9625942aa11253c5398da256a861a9d37c4b4aec
Gregory Goncharov <realgbto@gmail.com>
2023-10-26 07:51:38 +0300

    Wrong version of first parser

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ if response.status_code == 200:
     print("----Connected----")
     with open("data_about.html", "w", encoding="utf-8") as file:
         file.write(response.text)
+
+jdsfjfjdsfojdjdsfihsfihsfiudhfiushfhf9hfsdhiuhfidhshfisdhfihdsfhidsuhfuidshfiusdhfuidshfiuhdsfihdsfh
+hfsdhfidshfiudshfhdsf
+fsfhsdiohfoisehfousedhfewf
